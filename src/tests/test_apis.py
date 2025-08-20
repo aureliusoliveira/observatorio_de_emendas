de69ca807da63d25cb76caf1c81a0c9ee8b60fab
Aurelius <aurelius.oliveira@gmail.com>
2025-08-20 12:11:45 -0300

    Remove unused API classes and test files for EmendasParlamentaresAPI

--- src/tests/test_apis.py
+++
@@ -1,9 +0,0 @@
-from src.apis import EmendasParlamentaresAPI
-
-class TestEmendasParlamentaresAPI:
-    def test_build_url(self):
-        path = "emendas"
-        assert EmendasParlamentaresAPI(path)._build_url() == \
-            "https://api.portaldatransparencia.gov.br/api-de-dados/emendas"
-        
-    
