57e22152673327c75559d03c0e5ca82e7c448e18
Aurelius <aurelius.oliveira@gmail.com>
2025-08-26 20:22:19 -0300

    feat: Implement OrgaosSIAPEAPI and OrgaosSIAFIAPI classes with URL building methods

---
+++ src/portal_da_transparencia/apis/orgaos/orgaos_api.py
@@ -0,0 +1,9 @@
+from ..api_interface import PortalDaTransparenciaAPI
+
+class OrgaosSIAPEAPI(PortalDaTransparenciaAPI):
+   def _build_url(self) -> str:
+       return f"{self.base_endpoint}/orgaos-siape"
+
+class OrgaosSIAFIAPI(PortalDaTransparenciaAPI):
+   def _build_url(self) -> str:
+       return f"{self.base_endpoint}/orgaos-siafi"
